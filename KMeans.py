864760495b12464b3315e6209545dabe496d23fc
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-16 21:46:58 +0530

    graph using scatter plot

--- KMeans.py
+++ KMeans.py
@@ -1,6 +1,5 @@
-from sklearn import *
+from sklearn import linear_model, cluster
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from numpy import *
 import csv
 from io import open
@@ -14,9 +13,10 @@ with open(path_to_csv, newline='') as file:
         data.append([float(x) for x in row])
     data = array(data)
 
+sample_size = 100
 clf = linear_model.Ridge(alpha = .8)
 x = []
-for column in data[:, 1:-1].T:
+for column in data[:, 1:sample_size+1].T:
     clf.fit(data[:, 0].reshape(-1, 1), column)
     x.append([column[-1], clf.coef_[0]])
 
@@ -24,12 +24,10 @@ x = array(x)
 CENTROIDS = 10
 prediction = cluster.KMeans(n_clusters=CENTROIDS).fit_predict(x)
 
-for i in range(100):
-    print("{},{}".format(header[i+1], prediction[i]))
+for i in range(sample_size):
+    print("{},{}".format(header[i+1], prediction[i]+1))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-# plt.scatter(x[:,0], x[:,1], c=prediction)
-for x in data[:, 1:-1]:
-    ax.scatter(range(1, 101), range(1, 101), x, c=prediction)
+plt.subplot(111)
+for i in range(len(data)):
+    plt.scatter(repeat(array(i), sample_size), data[i, 1:sample_size+1], c=prediction)
 plt.show()
