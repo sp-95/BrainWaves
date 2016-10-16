d7016157e7cef8868831c2e5c43d4d69b73b4979
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-16 23:24:18 +0530

    Predict Test Data

--- KMeans.py
+++ KMeans.py
@@ -4,37 +4,67 @@ import numpy as np
 import csv
 from io import open
 
-# path_to_csv = './soc_gen_data/train.csv'
-path_to_csv = './soc_gen_data/test.csv'
-data = []
-with open(path_to_csv, newline='') as file:
+training_csv = './soc_gen_data/train.csv'
+with open(training_csv, newline='') as file:
     reader = csv.reader(file)
     header = next(reader)
+    training_data = []
     for row in reader:
-        data.append([float(x) for x in row])
-    data = np.array(data)
+        training_data.append([float(x) for x in row])
+    training_data = np.array(training_data)
 
 start = 1
 end = 100
 size = end - start + 1
-
 clf = linear_model.Ridge(alpha = .8)
 x = []
-for column in data[:, start:end+1].T:
-    clf.fit(data[:, 0].reshape(-1, 1), column)
+for column in training_data[:, start:end+1].T:
+    clf.fit(training_data[:, 0].reshape(-1, 1), column)
     x.append([column[-1], clf.coef_[0]])
-
 x = np.array(x)
-CENTROIDS = 10
-prediction = cluster.KMeans(n_clusters=CENTROIDS).fit_predict(x)
 
-for i in range(size):
-    print("{},{}".format(header[i+start], prediction[i]+1))
+CENTROIDS = 10
+kmeans = cluster.KMeans(n_clusters=CENTROIDS).fit(x)
+clusters = kmeans.labels_
+
+# print("Asset,Cluster")
+# for i in range(size):
+#     print("{},{}".format(header[i+start], clusters[i]+1))
 
 cmap = cm.jet
-pyplot.subplot(111)
+pyplot.subplot(211)
 for i in range(size):
-    pyplot.plot(data[:, 0], data[:, i+start], color=cmap(prediction[i]/CENTROIDS))
+    pyplot.plot(training_data[:, 0], training_data[:, i+start], color=cmap(clusters[i]/CENTROIDS))
+pyplot.title("Training Data")
+pyplot.ylabel("Stock Price")
+pyplot.xlabel("Time")
+
+test_csv = './soc_gen_data/test.csv'
+with open(test_csv, newline='') as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    test_data = []
+    for row in reader:
+        test_data.append([float(x) for x in row])
+    test_data = np.array(test_data)
+
+x = []
+for column in test_data[:, start:end+1].T:
+    clf.fit(test_data[:, 0].reshape(-1, 1), column)
+    x.append([column[-1], clf.coef_[0]])
+x = np.array(x)
+
+predictions = kmeans.predict(x)
+
+print("Asset,Cluster")
+for i in range(size):
+    print("{},{}".format(header[i+start], predictions[i]+1))
+
+cmap = cm.jet
+pyplot.subplot(212)
+for i in range(size):
+    pyplot.plot(test_data[:, 0], test_data[:, i+start], color=cmap(predictions[i]/CENTROIDS))
+pyplot.title("Test Data")
 pyplot.ylabel("Stock Price")
 pyplot.xlabel("Time")
 pyplot.show()
