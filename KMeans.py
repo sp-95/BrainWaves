9a595290e9c845cd20a07d89d0de4b39494c2332
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-16 21:55:34 +0530

    Generalizing variables

--- KMeans.py
+++ KMeans.py
@@ -13,10 +13,13 @@ with open(path_to_csv, newline='') as file:
         data.append([float(x) for x in row])
     data = array(data)
 
-sample_size = 100
+start = 1
+end = 100
+size = end - start + 1
+
 clf = linear_model.Ridge(alpha = .8)
 x = []
-for column in data[:, 1:sample_size+1].T:
+for column in data[:, start:end+1].T:
     clf.fit(data[:, 0].reshape(-1, 1), column)
     x.append([column[-1], clf.coef_[0]])
 
@@ -24,10 +27,10 @@ x = array(x)
 CENTROIDS = 10
 prediction = cluster.KMeans(n_clusters=CENTROIDS).fit_predict(x)
 
-for i in range(sample_size):
-    print("{},{}".format(header[i+1], prediction[i]+1))
+for i in range(size):
+    print("{},{}".format(header[i+start], prediction[i]+1))
 
 plt.subplot(111)
 for i in range(len(data)):
-    plt.scatter(repeat(array(i), sample_size), data[i, 1:sample_size+1], c=prediction)
+    plt.scatter(repeat(array(i), size), data[i, start:end+1], c=prediction)
 plt.show()
