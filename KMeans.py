45dea395d3949cfbb190087b847a4f994b7d0ec3
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-16 22:50:59 +0530

    Apply on Test data

--- KMeans.py
+++ KMeans.py
@@ -1,17 +1,18 @@
 from sklearn import linear_model, cluster
 from matplotlib import pyplot, cm
-from numpy import *
+import numpy as np
 import csv
 from io import open
 
-path_to_csv = './soc_gen_data/train.csv'
+# path_to_csv = './soc_gen_data/train.csv'
+path_to_csv = './soc_gen_data/test.csv'
 data = []
 with open(path_to_csv, newline='') as file:
     reader = csv.reader(file)
     header = next(reader)
     for row in reader:
         data.append([float(x) for x in row])
-    data = array(data)
+    data = np.array(data)
 
 start = 1
 end = 100
@@ -23,7 +24,7 @@ for column in data[:, start:end+1].T:
     clf.fit(data[:, 0].reshape(-1, 1), column)
     x.append([column[-1], clf.coef_[0]])
 
-x = array(x)
+x = np.array(x)
 CENTROIDS = 10
 prediction = cluster.KMeans(n_clusters=CENTROIDS).fit_predict(x)
 
@@ -33,5 +34,7 @@ for i in range(size):
 cmap = cm.jet
 pyplot.subplot(111)
 for i in range(size):
-    pyplot.plot(data[:, i+start], color=cmap(prediction[i]/CENTROIDS), label=header[i+start])
+    pyplot.plot(data[:, 0], data[:, i+start], color=cmap(prediction[i]/CENTROIDS))
+pyplot.ylabel("Stock Price")
+pyplot.xlabel("Time")
 pyplot.show()
